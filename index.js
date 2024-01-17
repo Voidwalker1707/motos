4988a640c310b8566d8a9f8a8419c869a2ecdd2f
voidwalker1707 <angeldavid3511@gmail.com>
2024-01-17 11:50:48 -0500

    actu

---
+++ index.js
@@ -0,0 +1,4 @@
+const imgs = ["imagenes/1.jpg","imagenes/3.JPG","imagenes/4.JPG","imagenes/5.JPG",
+"imagenes/6.JPG","imagenes/7.JPG","imagenes/8.JPG","imagenes/9.JPG","imagenes/10.JPG","imagenes/11.JPG","imagenes/12.JPG","imagenes/13.JPG","imagenes/14.JPG",
+"imagenes/15.JPG","imagenes/16.JPG","imagenes/17.JPG","imagenes/18.JPG","imagenes/19.JPG","imagenes/20.JPG","imagenes/21.JPG","imagenes/22.JPG","imagenes/Capture.JPG","imagenes/moto 1.jpg",
+"imagenes/pagina abajo.JPG","imagenes/pagina.JPG","","","","","",""]
